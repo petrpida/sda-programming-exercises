0dbe60134b8de11acbb965c7308439b6b04c8259
petrpida <petstu@email.cz>
2022-03-02 16:10:16 +0100

    count_letters and duplicates done

--- loops/count_letters.js
+++ loops/count_letters.js
@@ -1,3 +1,20 @@
 // Napište funkci pro zjištění a výpis počtu všech písmen ve slově.
 // Vstup: "aha", 
 // Výstup: "Písmeno A nalezeno 2x. Písmeno H nalezeno 1x."
+
+function countLetters(word) {
+    let output = ""
+    let firstTimers = []
+    let array = Array.from(word.split(""))
+
+    array.forEach((el) => {
+        if (!firstTimers.includes(el)) {
+        output = output + " Pismeno " + el.toUpperCase() + " nalezeno " + (word.split(el).length - 1) + "x." 
+        firstTimers.push(el)  
+        }
+    })
+    return output
+}
+
+console.log(countLetters("ahoooj"))
+console.log(countLetters("aha"))
--- loops/duplicated_items_in_array.js
+++ loops/duplicated_items_in_array.js
@@ -1,1 +1,16 @@
 // Napiště funkci findDup(), která pro zadané pole vrací všechny prvky, které se v daném poli opakují více než jednou.
+
+function findDup(array) {
+    let firstTimers = []
+    let dupArray = []
+
+    array.forEach(element => {
+        if (firstTimers.includes(element) && !dupArray.includes(element)) {
+            dupArray.push(element)
+        }
+        firstTimers.push(element)
+    });
+    return dupArray
+}
+
+console.log(findDup([1, 2, 2, 2, 3, 3, 4, 4, 5]))
