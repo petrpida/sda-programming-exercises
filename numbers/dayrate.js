3054253b13a8fbbcdeecb308611d71f68fec132d
petrpida <petstu@email.cz>
2022-03-02 12:22:58 +0100

    numbers done

--- numbers/dayrate.js
+++ numbers/dayrate.js
@@ -1,3 +1,9 @@
 // Napište funkci pro výpočet denní sazby vzhledem k hodinové sazbě.
 // Funkce bude přijímat hodinovou sazbu pracovníka a vrátí cenu pracovníka za den, když počítáme s 8hodinovou pracovní dobou.
 // Vstup: 89, Výstup: 712
+
+function countDailyRate (hourlyRate) {
+    return hourlyRate * 8
+}
+
+console.log(countDailyRate(89))
--- numbers/daysInBudget.js
+++ numbers/daysInBudget.js
@@ -2,3 +2,9 @@
 // Funkce bude přijímat rozpočet zaměstnavatele a hodinovou sazbu pracovníka
 // a vrátí maximální počet dní po které si může zaměstnavatel pracovníka dovolit, když počítáme s 8hodinovou pracovní dobou.
 // Vstup: 20000, 89, Výstup: 28
+
+function daysInBudget (budget, hourlyRate) {
+    return Math.floor(budget/ (hourlyRate * 8))
+}
+
+console.log(daysInBudget(20600, 89))
